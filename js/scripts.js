4a7707bdb5587a43bd4b0486d60d91b39eab96fe
Matej Navara <Matej.navara@gmail.com>
2017-12-08 04:20:55 +0000

    Extracted js from index and improved npm scripts

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$("#carousel-button").click(function(){
+    if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+        $("#carousel").carousel('pause');
+        $("#carousel-button").children("span").removeClass('fa-pause');
+        $("#carousel-button").children("span").addClass('fa-play');
+    }
+    else if ($("#carousel-button").children("span").hasClass('fa-play')){
+        $("#carousel").carousel('cycle');
+        $("#carousel-button").children("span").removeClass('fa-play');
+        $("#carousel-button").children("span").addClass('fa-pause');                    
+    }
+});
